6d70dbc2a04d2007e3aaee9dc24e26afd075c29c
Marcio Junior <marciodossantos.04@gmail.com>
2024-06-30 14:25:17 -0300

    contaTerminalV2

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -25,7 +25,7 @@ public class ContaTerminal {
         double saldo = scanner.nextDouble();
 
         // Exibe a mensagem com as informações inseridas
-        System.out.println( "Olá " + nomeCliente + ", obrigado por criar uma 1021conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+        System.out.println( "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
 
         scanner.close();
     
